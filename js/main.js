88dc306be11f2b7715c9d59f8b7a9520815b59a4
EduardoHdez <jhernandezg1403@gmail.com>
2026-02-17 16:34:35 -0600

    Añade links a iconos

--- js/main.js
+++ js/main.js
@@ -206,9 +206,13 @@ if (navbar_container) {
                                 </button>
 
                                 <i class="bi bi-person-fill fs-5"></i>
+
+                                <a href="../pages/comprador.html">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                                     </svg>
+                                </a>
+                                    
 
                                </div>
 
